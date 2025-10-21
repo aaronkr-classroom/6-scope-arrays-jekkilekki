a3d513fdae8b04b91deef25f3b9a74cad6096286
ththfql <jekkilekki@gmail.com>
2025-10-21 17:14:09 +0900

    Fin

---
+++ Week 7/local.c
@@ -0,0 +1,15 @@
+﻿// local.c (지역 변수)
+#include <stdio.h>
+
+int Sum(int x, int y) {
+	int result = x + y;
+	return result;
+}
+
+int main(void) {
+	int a = 3, b = 5;
+	result = Sum(a, b);
+	printf("%d + %d = %d", a, b, result);
+
+	return 0;
+}
